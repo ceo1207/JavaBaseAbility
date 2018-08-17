3d96d933086a2d2a52293c9525536fcbdd9820ad
ceo1207 <water799@126.com>
2018-08-17 11:05:43 +0800

    f

--- src/main/java/IoTest.java
+++ src/main/java/IoTest.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class IoTest {
+    public static void main(String[] args)
+    {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println(in.nextLine());
+//        System.out.println(in.nextInt());
+
+        try {
+//            FileInputStream fileIn = new FileInputStream("d");
+            File file = new File("d");
+            FileInputStream fileIn = new FileInputStream(file);
+            long leng = file.length();
+            byte[] data = new byte[(int)leng];
+            int a = fileIn.read(data);
+            System.out.println(a);
+            System.out.println(data);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
 }
